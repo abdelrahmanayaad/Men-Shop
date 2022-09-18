f76582a5b9807adef85d5bcce4c819c99c4861c4
abdelrahmanayaad <abdelrahmanayad74@gmail.com>
2022-09-18 22:38:58 +0200

    Back

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+        <Text>Hello World !</Text>
+      </View>
+    )
+  }
+}
+
+export default App
